4b2075d14510741dff9287bfcb819d57c91b7811
听寒 <10476912+pastrale@users.noreply.github.com>
2021-05-19 17:51:08 +0800

    用户条控制

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -33,9 +33,10 @@
                     </span>
                     <template #dropdown>
                         <el-dropdown-menu>
-                            <a href="https://github.com/lin-xin/vue-manage-system" target="_blank">
-                                <el-dropdown-item>项目仓库</el-dropdown-item>
-                            </a>
+<!--                            <a href="https://github.com/lin-xin/vue-manage-system" target="_blank">-->
+<!--                                <el-dropdown-item>项目仓库</el-dropdown-item>-->
+<!--                            </a>-->
+                            <el-dropdown-item> 登录用户名：{{username}}</el-dropdown-item>
                             <el-dropdown-item divided command="loginout">退出登录</el-dropdown-item>
                         </el-dropdown-menu>
                     </template>
